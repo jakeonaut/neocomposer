3f561c723c7353a59032f5beb258fa4ee2dac292
jake <jakeonaut@gmail.com>
2025-11-02 23:56:13 -0800

    this is sick as hell

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,45 @@
-import React, { useCallback, useEffect, useState } from 'react';
+import React, { useCallback, useEffect, useMemo, useState } from 'react';
 import './App.css';
 import { Soundfont2Sampler } from './smplr/soundfont2'
 import { SoundFont2 } from "soundfont2";
+import { SampleStart } from './smplr/player/types';
+
+const keyboardPianoKeys = new Map(Object.entries({
+  'a': 'C4',
+  's': 'D4',
+  'd': 'E4',
+  'f': 'F4',
+  'g': 'G4',
+  'h': 'A4',
+  'j': 'B4',
+  'k': 'C5',
+  'l': 'D5',
+  ';': 'E5',
+  "'": 'F5',
+}));
+
+type MidiNote = string;
+type MidiBeat = number;
+type Composition = {
+  [id: MidiBeat]: {
+    [id: MidiNote]: SampleStart | undefined
+  }
+}
+const pianoRollKeys: MidiNote[] = ['C4', 'D4', 'E4', 'F4', 'G4', 'A4', 'B4', 'C5'].reverse();
+const pianoRollBeats = [1, 2, 3, 4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20];
 
 function App() {
   const [context] = useState(new AudioContext());
   const [sf2Sampler, setSf2Sampler] = useState<Soundfont2Sampler | undefined>();
+  const [babyDanceFrame, setBabyDanceFrame] = useState(0);
+  const [composition, setComposition] = useState<Composition>({});
   const handleKeyDown = useCallback((e: KeyboardEvent) => {
     if (e.repeat) { return; }
 
-    const pianoKeys = new Map(Object.entries({
-      'a': 'C4',
-      's': 'D4',
-      'd': 'E4',
-      'f': 'F4',
-      'g': 'G4',
-      'h': 'A4',
-      'j': 'B4',
-      'k': 'C5',
-      'l': 'D5',
-      ';': 'E5',
-      "'": 'F5',
-    }));
-    const playedNote = pianoKeys.has(e.key) ? pianoKeys.get(e.key) : undefined;
-    console.log(playedNote);
+    const playedNote = keyboardPianoKeys.has(e.key) ? keyboardPianoKeys.get(e.key) : undefined;
     if (!sf2Sampler || !playedNote) { return; }
     sf2Sampler.start({ note: playedNote, time: context.currentTime, duration: 0.25 });
+    setBabyDanceFrame((prev) => prev < 3 ? prev+1 : 0);
   }, [sf2Sampler]);
   useEffect(() => {
     document.addEventListener('keydown', handleKeyDown);
@@ -65,25 +79,78 @@ function App() {
           const now = context.currentTime;
           ["C4", "E4", "G4", "C5"].forEach((note, i) => {
             const duration = 0.25;
-            soundfont2Sampler.start({ note, time: now + i * duration, duration });
+            soundfont2Sampler.start({ note, time: now + i * duration, duration, onStart: () => {setBabyDanceFrame((prev) => prev < 3 ? prev+1 : 0);} });
           });
         });
         setSf2Sampler(soundfont2Sampler);
     }
   }, []);
 
+  const beatWidth = 32;
   return (
-    <div className="App">
-      <label>Choose soundfont2: </label>
-      <input type="file" accept=".sf2" onChange={onUploadSf2} />
-      {sf2Sampler && (<>
-        <label>Select instrument: </label>
-        <select onChange={((e) => {
-          sf2Sampler.loadInstrument(e.target.value);
-        })}>
-          {sf2Sampler.instrumentNames.map((name) => <option value={name}>{name}</option>)}
-        </select>
-      </>)}
+    <div className="App" style={{ display: 'flex', flexDirection: 'column', height: '100%', }}>
+      <div style={{ display: "flex", gap: 4, outline: '1px solid black', backgroundColor: '#8cb4b0', margin: 2,}}>
+        <div>
+          <img src="trans.png" style={{
+            margin: "6px 8px 10px 10px",
+            width: 20,
+            height: 20,
+            imageRendering: 'pixelated',
+            transform: 'scale(2.0)',
+            backgroundImage: "url('baby_dance_sheet.png')",
+            backgroundPosition: `${babyDanceFrame*-20}px 0px`,
+          }} />
+        </div>
+        <div style={{ display: 'flex', gap: 8, alignItems: 'center', }}>
+          <label htmlFor="sf-uploader">Choose soundfont2: </label>
+          <input id="sf-uploader" type="file" accept=".sf2" onChange={onUploadSf2} />
+          {sf2Sampler && (<>
+            <label htmlFor="sf-instrument-select">Select instrument: </label>
+            <select id="sf-instrument-select" onChange={((e) => {
+              sf2Sampler.loadInstrument(e.target.value);
+            })}>
+              {sf2Sampler.instrumentNames.map((name) => <option value={name} key={name}>{name}</option>)}
+            </select>
+            <span> * Play with: </span><pre>asdfjkl;wetyuop</pre>
+          </>)}
+        </div>
+      </div>
+      <div style={{ flexGrow: 1, display: 'flex', flexDirection: 'column' }}>
+        <div style={{ display: 'flex', }}>
+          <div key="empty" style={{ width: beatWidth}}>&nbsp;</div>
+          {pianoRollBeats.map((beat) => (<div key={beat} style={{ width: beatWidth}}>{beat}</div>))}
+        </div>
+        {pianoRollKeys.map((midiNote) => (
+          <div style={{ display: 'flex' }}>
+            <div key={midiNote} style={{ width: beatWidth}}>{midiNote}</div>
+            {pianoRollBeats.map((midiBeat) => (
+              <div key={midiBeat} className='hoverable'
+                style={{
+                  outline: '1px solid lightgray',
+                  width: beatWidth,
+                  cursor: 'pointer',
+                  ...(composition[midiBeat]?.[midiNote] !== undefined ? {backgroundColor: '#8cb4b0'}  : {}),
+                }}
+                onClick={(e) => {
+                  e.preventDefault();
+                  e.stopPropagation();
+                  setComposition((prev) => {
+                    if (!prev[midiBeat]) {
+                      prev[midiBeat] = {};
+                    }
+                    console.log("this:", prev[midiBeat][midiNote]);
+                    if (prev[midiBeat][midiNote]) {
+                      delete prev[midiBeat][midiNote];
+                    } else {
+                      prev[midiBeat][midiNote] = { note: midiNote, duration: 0.25 };
+                    }
+                    return {...prev};
+                  })
+                }}/>
+            ))}
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
